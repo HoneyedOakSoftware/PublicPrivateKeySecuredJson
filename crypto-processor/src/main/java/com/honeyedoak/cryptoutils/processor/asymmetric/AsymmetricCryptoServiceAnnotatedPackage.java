b19dc589a87f78e6263caf53346979459bee4330
Pieter Van Eeckhout <vaneeckhout.pieter@gmail.com>
2018-07-28 00:45:10 +0200

    migrated cryto processor

---
+++ crypto-processor/src/main/java/com/honeyedoak/cryptoutils/processor/asymmetric/AsymmetricCryptoServiceAnnotatedPackage.java
@@ -0,0 +1,141 @@
+package com.honeyedoak.cryptoutils.processor.asymmetric;
+
+import com.squareup.javapoet.*;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.annotation.processing.Filer;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.PackageElement;
+import java.io.IOException;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class AsymmetricCryptoServiceAnnotatedPackage {
+
+	private PackageElement annotatedPackageElement;
+	private String serviceName;
+	private String algorithm;
+	private int keySize;
+
+	public AsymmetricCryptoServiceAnnotatedPackage(PackageElement packageElement) throws IllegalArgumentException {
+		this.annotatedPackageElement = packageElement;
+		com.honyedoak.ppksecuredjson.crypto.annotation.AsymmetricCryptoService annotation = packageElement.getAnnotation(com.honyedoak.ppksecuredjson.crypto.annotation.AsymmetricCryptoService.class);
+		serviceName = annotation.serviceName();
+		algorithm = annotation.algorithm();
+		keySize = annotation.keySize();
+
+		if (StringUtils.isEmpty(serviceName)) {
+			throw new IllegalArgumentException(
+					String.format("serviceName() in @%s for class %s is null or empty! that's not allowed",
+							com.honyedoak.ppksecuredjson.crypto.annotation.AsymmetricCryptoService.class.getSimpleName(), packageElement.getQualifiedName().toString()));
+		}
+
+		if (StringUtils.isEmpty(algorithm)) {
+			throw new IllegalArgumentException(
+					String.format("algorithm() in @%s for class %s is null or empty! that's not allowed",
+							com.honyedoak.ppksecuredjson.crypto.annotation.AsymmetricCryptoService.class.getSimpleName(), packageElement.getQualifiedName().toString()));
+		}
+
+		if (keySize < 1) {
+			throw new IllegalArgumentException(
+					String.format("keySize() in @%s for class %s is zero or negative! that's not allowed",
+							com.honyedoak.ppksecuredjson.crypto.annotation.AsymmetricCryptoService.class.getSimpleName(), packageElement.getQualifiedName().toString()));
+		}
+	}
+
+	public PackageElement getAnnotatedPackageElement() {
+		return annotatedPackageElement;
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public String getAlgorithm() {
+		return algorithm;
+	}
+
+	public int getKeySize() {
+		return keySize;
+	}
+
+	public void generateCode(Filer filer) throws IOException {
+		FieldSpec asymmetricCryptoUtils = FieldSpec.builder(com.honyedoak.ppksecuredjson.crypto.annotation.AsymmetricCryptoService.class, "asymmetricCryptoUtils", Modifier.PRIVATE, Modifier.FINAL)
+				.build();
+
+		MethodSpec constructor = MethodSpec.constructorBuilder()
+				.addModifiers(Modifier.PUBLIC)
+				.addCode("this.asymmetricCryptoUtils = new $T($S, $L);", com.honeyedoak.ppksecuredjson.crypto.AsymmetricCryptoServiceImpl.class, algorithm, keySize)
+				.build();
+
+		MethodSpec generateKeyPair = MethodSpec.methodBuilder("generateKeyPair")
+				.addAnnotation(Override.class)
+				.addModifiers(Modifier.PUBLIC)
+				.addException(com.honeyedoak.ppksecuredjson.crypto.exception.CryptoException.class)
+				.returns(KeyPair.class)
+				.addCode("return this.asymmetricCryptoUtils.generateKeyPair();")
+				.build();
+
+		MethodSpec decodePrivateKey = MethodSpec.methodBuilder("decodePrivateKey")
+				.addAnnotation(Override.class)
+				.addModifiers(Modifier.PUBLIC)
+				.addException(com.honeyedoak.ppksecuredjson.crypto.exception.CryptoException.class)
+				.returns(PrivateKey.class)
+				.addParameter(byte[].class, "encodedKey")
+				.addCode("return asymmetricCryptoUtils.decodePrivateKey(encodedKey);")
+				.build();
+
+		MethodSpec decodePublicKey = MethodSpec.methodBuilder("decodePublicKey")
+				.addAnnotation(Override.class)
+				.addModifiers(Modifier.PUBLIC)
+				.addException(com.honeyedoak.ppksecuredjson.crypto.exception.CryptoException.class)
+				.returns(PublicKey.class)
+				.addParameter(byte[].class, "encodedKey")
+				.addCode("return asymmetricCryptoUtils.decodePublicKey(encodedKey);")
+				.build();
+
+		MethodSpec decrypt = MethodSpec.methodBuilder("decrypt")
+				.addAnnotation(Override.class)
+				.addModifiers(Modifier.PUBLIC)
+				.addException(com.honeyedoak.ppksecuredjson.crypto.exception.CryptoException.class)
+				.returns(byte[].class)
+				.addParameter(byte[].class, "payload")
+				.addParameter(Key.class, "key")
+				.addCode("return asymmetricCryptoUtils.decrypt(payload, key);")
+				.build();
+
+		MethodSpec encrypt = MethodSpec.methodBuilder("encrypt")
+				.addAnnotation(Override.class)
+				.addModifiers(Modifier.PUBLIC)
+				.addException(com.honeyedoak.ppksecuredjson.crypto.exception.CryptoException.class)
+				.returns(byte[].class)
+				.addParameter(byte[].class, "payload")
+				.addParameter(Key.class, "key")
+				.addCode("return asymmetricCryptoUtils.encrypt(payload, key);")
+				.build();
+
+		AnnotationSpec generated = AnnotationSpec.builder(javax.annotation.Generated.class)
+				.addMember("value", "$S", CryptoutilsAsymmetricCryptoServiceProcessor.class.getName())
+				.build();
+
+		TypeSpec asymmetricCryptoService = TypeSpec.classBuilder(serviceName)
+				.addAnnotation(generated)
+				.addModifiers(Modifier.PUBLIC)
+				.addSuperinterface(com.honeyedoak.ppksecuredjson.crypto.AsymmetricCryptoService.class)
+				.addField(asymmetricCryptoUtils)
+				.addAnnotation(org.springframework.stereotype.Service.class)
+				.addMethod(constructor)
+				.addMethod(generateKeyPair)
+				.addMethod(decodePrivateKey)
+				.addMethod(decodePublicKey)
+				.addMethod(decrypt)
+				.addMethod(encrypt)
+				.build();
+
+		JavaFile.builder(annotatedPackageElement.getQualifiedName().toString(), asymmetricCryptoService)
+				.build()
+				.writeTo(filer);
+	}
+}
---
+++ crypto-processor/src/main/java/com/honeyedoak/cryptoutils/processor/symmetric/SymmetricCryptoServiceAnnotatedPackage.java
@@ -0,0 +1,116 @@
+package com.honeyedoak.cryptoutils.processor.symmetric;
+
+import com.squareup.javapoet.*;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.annotation.processing.Filer;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.PackageElement;
+import java.io.IOException;
+
+public class SymmetricCryptoServiceAnnotatedPackage {
+
+	private PackageElement annotatedPackageElement;
+	private String serviceName;
+	private String algorithm;
+
+	public SymmetricCryptoServiceAnnotatedPackage(PackageElement packageElement) throws IllegalArgumentException {
+		this.annotatedPackageElement = packageElement;
+		com.honyedoak.ppksecuredjson.crypto.annotation.SymmetricCryptoService annotation = packageElement.getAnnotation(com.honyedoak.ppksecuredjson.crypto.annotation.SymmetricCryptoService.class);
+		serviceName = annotation.serviceName();
+		algorithm = annotation.algorithm();
+
+		if (StringUtils.isEmpty(serviceName)) {
+			throw new IllegalArgumentException(
+					String.format("serviceName() in @%s for class %s is null or empty! that's not allowed",
+							com.honyedoak.ppksecuredjson.crypto.annotation.SymmetricCryptoService.class.getSimpleName(), packageElement.getQualifiedName().toString()));
+		}
+
+		if (StringUtils.isEmpty(algorithm)) {
+			throw new IllegalArgumentException(
+					String.format("algorithm() in @%s for class %s is null or empty! that's not allowed",
+							com.honyedoak.ppksecuredjson.crypto.annotation.SymmetricCryptoService.class.getSimpleName(), packageElement.getQualifiedName().toString()));
+		}
+	}
+
+	public PackageElement getAnnotatedPackageElement() {
+		return annotatedPackageElement;
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public String getAlgorithm() {
+		return algorithm;
+	}
+
+	public void generateCode(Filer filer) throws IOException {
+		String symmetricCryptoServiceFieldName = "symmetricCryptoService";
+
+		FieldSpec symmetricCryptoUtils = FieldSpec.builder(com.honyedoak.ppksecuredjson.crypto.annotation.SymmetricCryptoService.class, symmetricCryptoServiceFieldName, Modifier.PRIVATE, Modifier.FINAL)
+				.build();
+
+		MethodSpec constructor = MethodSpec.constructorBuilder()
+				.addModifiers(Modifier.PUBLIC)
+				.addCode("this.$L = new $T($S);", symmetricCryptoServiceFieldName, com.honeyedoak.ppksecuredjson.crypto.SymmetricCryptoServiceImpl.class, algorithm)
+				.build();
+
+		MethodSpec decrypt = MethodSpec.methodBuilder("decrypt")
+				.addAnnotation(Override.class)
+				.addModifiers(Modifier.PUBLIC)
+				.returns(byte[].class)
+				.addParameter(byte[].class, "payload")
+				.addParameter(String.class, "password")
+				.addCode("return this.$L.decrypt(payload, password);", symmetricCryptoServiceFieldName)
+				.build();
+
+		MethodSpec decryptChar = MethodSpec.methodBuilder("decrypt")
+				.addAnnotation(Override.class)
+				.addModifiers(Modifier.PUBLIC)
+				.returns(byte[].class)
+				.addParameter(byte[].class, "payload")
+				.addParameter(char[].class, "password")
+				.addCode("return this.$L.decrypt(payload, password);", symmetricCryptoServiceFieldName)
+				.build();
+
+		MethodSpec encrypt = MethodSpec.methodBuilder("encrypt")
+				.addAnnotation(Override.class)
+				.addModifiers(Modifier.PUBLIC)
+				.returns(byte[].class)
+				.addParameter(byte[].class, "payload")
+				.addParameter(String.class, "password")
+				.addCode("return this.$L.encrypt(payload, password);", symmetricCryptoServiceFieldName)
+				.build();
+
+		MethodSpec encryptChar = MethodSpec.methodBuilder("encrypt")
+				.addAnnotation(Override.class)
+				.addModifiers(Modifier.PUBLIC)
+				.returns(byte[].class)
+				.addParameter(byte[].class, "payload")
+				.addParameter(char[].class, "password")
+				.addCode("return this.$L.encrypt(payload, password);", symmetricCryptoServiceFieldName)
+				.build();
+
+		AnnotationSpec generated = AnnotationSpec.builder(javax.annotation.Generated.class)
+				.addMember("value", "$S", com.honeyedoak.cryptoutils.processor.asymmetric.CryptoutilsAsymmetricCryptoServiceProcessor.class.getName())
+				.build();
+
+		TypeSpec asymmetricCryptoService = TypeSpec.classBuilder(serviceName)
+				.addAnnotation(generated)
+				.addModifiers(Modifier.PUBLIC)
+				.addSuperinterface(com.honyedoak.ppksecuredjson.crypto.annotation.SymmetricCryptoService.class)
+				.addAnnotation(org.springframework.stereotype.Service.class)
+				.addField(symmetricCryptoUtils)
+				.addMethod(constructor)
+				.addMethod(decrypt)
+				.addMethod(decryptChar)
+				.addMethod(encrypt)
+				.addMethod(encryptChar)
+				.build();
+
+		JavaFile.builder(annotatedPackageElement.getQualifiedName().toString(), asymmetricCryptoService)
+				.build()
+				.writeTo(filer);
+	}
+}
