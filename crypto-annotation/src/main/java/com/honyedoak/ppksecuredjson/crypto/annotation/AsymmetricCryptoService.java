fff7aac3c99fef4872f3f29022ac4e86e4f6d4fd
Pieter Van Eeckhout <vaneeckhout.pieter@gmail.com>
2018-07-27 16:44:06 +0200

    migrated crypto annotation

---
+++ crypto-annotation/src/main/java/com/honyedoak/ppksecuredjson/crypto/annotation/AsymmetricCryptoService.java
@@ -0,0 +1,17 @@
+package com.honyedoak.ppksecuredjson.crypto.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PACKAGE)
+public @interface AsymmetricCryptoService {
+
+	String serviceName() default "AsymmetricCryptoService";
+
+	String algorithm() default "RSA";
+
+	int keySize() default 4096;
+}
